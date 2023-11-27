27878a9560a1d5d3f1d7cd0e66ce27a61b14ddeb
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-11-27 08:45:12 +0530

    Update AbstractFactoryTest.java

--- src/main/java/designpattern/abstractfactory/AbstractFactoryTest.java
+++ src/main/java/designpattern/abstractfactory/AbstractFactoryTest.java
@@ -1,1 +1,26 @@
 
+package designpattern.abstractfactory;
+
+public class AbstractFactoryTest {
+
+	public static void main(String[] args) {
+		PhoneFactory phoneFactory = (PhoneFactory) FactoryBuilder.getFactory(FactoryType.PHONE_FACTORY);
+		Motorola phone =  (Motorola) phoneFactory.makePhone(PhoneType.MOTOROLA);
+		System.out.println(phone.getModel());
+		System.out.println(phone.getVendorName());
+		System.out.println();
+		
+		ComputerFactory computerFactory = (ComputerFactory) FactoryBuilder.getFactory(FactoryType.COMPUTER_FACTORY);
+		Dell computer = (Dell) computerFactory.makeComputer(ComputerType.DELL);
+		System.out.println(computer.getModel());
+		System.out.println(computer.getVendorName());
+		System.out.println();
+		
+		LaptopFactory laptopFactory = (LaptopFactory) FactoryBuilder.getFactory(FactoryType.LAPTOP_FACTORY);
+		HP laptop = (HP) laptopFactory.makeLaptop(LaptopType.HP);
+		System.out.println(laptop.getModel());
+		System.out.println(laptop.getVendorName());
+		System.out.println();
+	}
+}
+
