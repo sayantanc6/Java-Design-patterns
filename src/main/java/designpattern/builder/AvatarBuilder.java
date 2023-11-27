59b0029d4b9246b8fad2d3c2d276a775581ca3dc
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-11-27 19:45:30 +0530

    Create AvatarBuilder.java

---
+++ src/main/java/designpattern/builder/AvatarBuilder.java
@@ -0,0 +1,37 @@
+package designpattern.builder;
+
+public class AvatarBuilder {
+
+	private GlassType glassType;
+	private HairType hairType;
+	Personality personality;
+	
+	public AvatarBuilder() {
+		super();
+	}
+
+	public AvatarBuilder buildGlassType(GlassType glassType) {
+		this.glassType = glassType;
+		return this;
+	}
+	
+	public AvatarBuilder buildHairType(HairType hairType) {
+		this.hairType = hairType;
+		return this;
+	}
+	
+	public AvatarBuilder buildPersonality(Personality personality) {
+		this.personality = personality;
+		return this;
+	}
+	
+	public AvatarBuilder build() {
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "glassType=" + glassType + ", hairType=" + hairType + ", personality=" + personality
+				+ "\n";
+	}
+}
