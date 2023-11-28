6f546cb9381b3331d9bd3dc9e2a73e2841d2733e
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-11-28 10:30:03 +0530

    Create ExecutiveVicePresidentImpl.java

---
+++ src/main/java/designpattern/facade_strategy/ExecutiveVicePresidentImpl.java
@@ -0,0 +1,86 @@
+package designpattern.facade_strategy;
+
+public class ExecutiveVicePresidentImpl implements ExecutiveVicePresident {
+	
+	private DesignationTypes designation;
+	private String joblevel;
+	private String projectCode;
+	private int employeeID;
+	private String employeeName;
+	
+	public ExecutiveVicePresidentImpl(DesignationTypes designation, String joblevel, String projectCode, int employeeID,
+			String employeeName) {
+		super();
+		this.designation = designation;
+		this.joblevel = joblevel;
+		this.projectCode = projectCode;
+		this.employeeID = employeeID;
+		this.employeeName = employeeName;
+	}
+
+	public ExecutiveVicePresidentImpl() {
+	}
+
+	@Override
+	public DesignationTypes getDesignation() {
+		return this.designation;
+	}
+
+	@Override
+	public String getJobLevel() {
+		return this.joblevel;
+	}
+
+	@Override
+	public String getProjectCode() {
+		return this.projectCode;
+	}
+
+	@Override
+	public int getEmployeeID() {
+		return this.employeeID;
+	}
+
+	@Override
+	public String getEmployeeName() {
+		return this.employeeName;
+	}
+
+	@Override
+	public void workAsEVP(InfosysEmployee employee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setDesignation(DesignationTypes designation) {
+		this.designation = designation;
+	}
+
+	@Override
+	public void setJobLevel(String joblevel) {
+		this.joblevel = joblevel;
+	}
+
+	@Override
+	public void setProjectCode(String projectCode) {
+		this.projectCode = projectCode;
+	}
+
+	@Override
+	public void setEmployeeID(int employeeID) {
+		this.employeeID = employeeID;
+	}
+
+	@Override
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	@Override
+	public String toString() {
+		return "ExectiveVicePresidentImpl [designation=" + designation + ", joblevel=" + joblevel + ", projectCode="
+				+ projectCode + ", employeeID=" + employeeID + ", employeeName=" + employeeName + "]";
+	}
+
+}
