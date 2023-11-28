77b8a767098598b6cc900ba4c88ddde40c005096
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-11-28 09:31:22 +0530

    Create Car.java

---
+++ src/main/java/designpattern/Car.java
@@ -0,0 +1,12 @@
+package designpattern.decorator;
+
+public interface Car {
+	
+	int getEnginesize();
+	String getEngineTypeString();
+	int getCylindernumber();
+	int getMaxpower();
+	int getMaxtorque();
+	int getgGearbox();
+	int getWeight();
+}
