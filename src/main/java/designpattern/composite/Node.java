d49bdf3a109ec461f7aba6e964d0400e23ed329c
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-11-28 09:21:57 +0530

    Create Node.java

---
+++ src/main/java/designpattern/composite/Node.java
@@ -0,0 +1,16 @@
+package designpattern.composite;
+
+public abstract class Node {
+
+	void addChild(Node node) {
+		
+	}
+	
+	void removeChild(Node node) {
+		
+	}
+	
+	String displayNode() {
+		return null;
+	}
+}
